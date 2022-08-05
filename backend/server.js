113d33e244fca995b1ee5ed4193a5b10f813239e
Chris Young <chris_young@tutanota.com>
2022-08-05 16:42:50 +1200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const articles = require("./dummydata/articles");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const server = express();
+
+
+server.get('/', (req,res) => {
+    res.send("Oooh Weee Oooh I look just like Buddy Holly!");
+});
+
+server.get('/api/articles', (req,res) => {
+    res.json(articles);
+});
+
+server.get('/api/articles/:id', (req,res) => {
+    const article = articles.find((n) => n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+});
+
+server.listen(PORT, console.log('server is working and listening PORT ${PORT}'));
+//dogs
